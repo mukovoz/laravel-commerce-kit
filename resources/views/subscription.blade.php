b6c29fe314877a7f1cfbc9e5d0bf8f395c599515
oleg <codemorgan@gmail.com>
2026-05-26 21:38:41 +0300

    fix

--- resources/views/subscription.blade.php
+++ resources/views/subscription.blade.php
@@ -1,1 +1,11 @@
-{!! file_get_contents(config('platforms.apps_manager.script_url') . '?url=' . $site->url) !!}
+@php
+    try {
+        $script = file_get_contents(config('platforms.apps_manager.script_url') . '?url=' . $site->url);
+        if ($script === false) {
+            throw new RuntimeException('Error while getting subscription information');
+        }
+    } catch (Throwable $e) {
+        throw new RuntimeException('Error while getting subscription information', previous: $e);
+    }
+@endphp
+{!! $script !!}
